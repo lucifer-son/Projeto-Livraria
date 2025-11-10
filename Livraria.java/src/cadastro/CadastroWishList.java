e004b5d2b98dcbd209245d36be9969d6234def60
joaoluc90 <lucascosme.9090@outlook.com>
2025-11-10 01:16:25 -0300

    Create CadastroWishList.java

---
+++ Livraria.java/src/cadastro/CadastroWishList.java
@@ -0,0 +1,52 @@
+package cadastro;
+
+import model.WishList;
+import repositorio.WishListRepositorioArray;
+import excecoes.EntidadeJaExistenteExcecao;
+import excecoes.EntidadeNaoEncontradaExcecao;
+
+import java.util.List;
+
+public class CadastroWishList {
+
+    private final WishListRepositorioArray repositorio;
+
+    public CadastroWishList() {
+        this.repositorio = new WishListRepositorioArray();
+    }
+
+    public void cadastrar(WishList wishList) throws EntidadeJaExistenteExcecao {
+        if (repositorio.buscarPorId(wishList.getId()) != null) {
+            throw new EntidadeJaExistenteExcecao(wishList.getId(), "Já existe uma WishList com o ID " + wishList.getId());
+        }
+        repositorio.inserir(wishList);
+    }
+
+    public void atualizar(WishList wishList) throws EntidadeNaoEncontradaExcecao {
+        if (repositorio.buscarPorId(wishList.getId()) == null) {
+            throw new EntidadeNaoEncontradaExcecao(wishList.getId(), "WishList não encontrada para atualização.");
+        }
+        repositorio.atualizar(wishList);
+    }
+
+    public void remover(String id) throws EntidadeNaoEncontradaExcecao {
+        if (repositorio.buscarPorId(id) == null) {
+            throw new EntidadeNaoEncontradaExcecao(id, "WishList não encontrada para remoção.");
+        }
+        repositorio.deletarPorId(id);
+    }
+
+    public WishList buscarPorId(String id) {
+        return repositorio.buscarPorId(id);
+    }
+
+    public List<WishList> buscarTodos() {
+        return repositorio.buscarTodos();
+    }
+
+    public void listar() {
+        for (WishList wishList : repositorio.buscarTodos()) {
+            System.out.println(wishList);
+        }
+    }
+}
