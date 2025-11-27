e1db45f381dea5ef025ed70ef8099d02152f6a30
Mari-olima <mariana.soliver2002@gmail.com>
2025-11-26 23:54:50 -0300

    Update LivrosController.java

--- Livraria.java/src/GUI/controladores/LivrosController.java
+++ Livraria.java/src/GUI/controladores/LivrosController.java
@@ -1,7 +1,7 @@
 package GUI.controladores;
 
 
-import model.Livro; // ajuste conforme seu pacote model
+import model.Livro; 
 import GUI.BaseController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -22,7 +22,7 @@ public class LivrosController extends BaseController {
 
     @FXML
     public void initialize() {
-        // configurar colunas (assumindo getters getId, getTitulo...)
+       
 
         colTitulo.setCellValueFactory(cell -> new javafx.beans.property.SimpleStringProperty(cell.getValue().getTitulo()));
         colPreco.setCellValueFactory(cell -> new javafx.beans.property.SimpleObjectProperty<>(cell.getValue().getPreco()));
@@ -30,9 +30,7 @@ public class LivrosController extends BaseController {
 
         livrosTable.setItems(livros);
 
-        // TODO: carregar lista via LivroService (ex: livros.addAll(LivroService.listar()))
-        // placeholder exemplo:
-        // livros.add(new Livro(1, "Exemplo", "Autor X", 39.90, 10));
+       
 
         livrosTable.getSelectionModel().selectedItemProperty().addListener((obs, oldV, newV) -> {
             if (newV != null) detalheArea.setText(buildDetalhes(newV));
@@ -47,7 +45,7 @@ public class LivrosController extends BaseController {
     @FXML public void addToCart() {
         Livro sel = livrosTable.getSelectionModel().getSelectedItem();
         if (sel != null) {
-            // TODO: integrar com CarrinhoService
+            
             System.out.println("Adicionado ao carrinho: " + sel.getTitulo());
         }
     }
@@ -55,7 +53,7 @@ public class LivrosController extends BaseController {
     @FXML public void verAvaliacoes() {
         Livro sel = livrosTable.getSelectionModel().getSelectedItem();
         if (sel != null) {
-            // pode abrir modal com avaliações -> simplificado aqui:
+            
             Alert a = new Alert(Alert.AlertType.INFORMATION, "Mostrando avaliações (placeholder) para: " + sel.getTitulo(), ButtonType.OK);
             a.setHeaderText("Avaliações");
             a.showAndWait();
@@ -64,3 +62,4 @@ public class LivrosController extends BaseController {
 
     @FXML public void goBack() { navigate("menu"); }
 }
+
